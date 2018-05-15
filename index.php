facb4c0230371f06f743997e1a53dc3755803441
DavideL90 <davide.lecci@icloud.com>
2018-05-15 15:01:12 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>Ciao <?php echo $argv[0].$argv[1]; ?></h1>
+  </body>
+</html>
